59ef606bcf8d364df4792ddc83d8e46ef35c17e9
lineasrectas2332 <rectaslineas@gmail.com>
2026-02-08 17:55:08 -0300

    fix: base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/qora-data-/',
   plugins: [react()],
-  base: '/qora-data-/', // 👈 nombre EXACTO del repo
 })
