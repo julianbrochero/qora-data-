a807e040520cbf548b4bf08444b037e0c6ab5cdb
julianbrochero <brocherojulian72@gmail.com>
2026-03-27 16:11:01 -0300

    feat: initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  // base: '/qora-data-/', // 👈 Comentado porque Vercel lo despliega en la raíz, no en una subcarpeta
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
